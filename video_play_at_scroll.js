1d7099a82cb1790fa2e0c3161e3ee41edf89e6ad
Andy Barry <abarry@gmail.com>
2017-02-19 10:08:51 -0500

    Adds javascript to make the videos only play when scrolled into view.  Also resets videos to the beginning when they are first scrolled into view.

---
+++ video_play_at_scroll.js
@@ -0,0 +1,42 @@
+var videos = document.getElementsByTagName("video"),
+    videoPlayAtFraction = 0.8;
+    function checkScroll() {
+
+        for(var i = 0; i < videos.length; i++) {
+
+            var video = videos[i];
+            
+            if (fractionScrolledIntoView(video) > videoPlayAtFraction) {
+                if (video.paused) {
+                    video.currentTime = 0
+                }
+                video.play();
+            } else {
+                video.pause();
+            }
+        }
+    }
+
+    function fractionScrolledIntoView(el) {
+        var elemTop = el.getBoundingClientRect().top;
+        var elemBottom = el.getBoundingClientRect().bottom;
+        var elemHeight = elemBottom - elemTop
+
+        if (elemTop > 0) {
+            topFrac = 1
+        } else {
+            topFrac = (elemTop + elemHeight) / elemHeight
+        }
+        
+        // Off the bottom of the screen
+        bottomFrac = 1 + (window.innerHeight - elemBottom) / elemHeight
+        bottomFrac = Math.max(0, bottomFrac)
+        bottomFrac = Math.min(1, bottomFrac)
+        
+        
+        fraction = Math.min(topFrac, bottomFrac)
+        return fraction
+    }
+
+    window.addEventListener('scroll', checkScroll, false);
+    window.addEventListener('resize', checkScroll, false);
