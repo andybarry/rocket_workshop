a690aec8c7268a5cb8ca7038958ce2f55d2e1348
RobbieKipp <133887343+RobbieKipp@users.noreply.github.com>
2025-12-03 10:39:25 -0500

    drone ide header

--- drone_interactive_instruction/src/App.jsx
+++ drone_interactive_instruction/src/App.jsx
@@ -3,6 +3,14 @@ import ResizablePanels from './components/ResizablePanels'
 function App() {
   return (
     <div className="app">
+      <header className="app-header">
+        <div className="app-header-left">
+          <span className="app-header-text">Robotics Workshop</span>
+          <span className="app-header-drone">Drone IDE</span>
+        </div>
+        <span className="app-header-stage">STAGE ONE EDUCATION</span>
+      </header>
+      <div className="app-header-bar"></div>
       <ResizablePanels />
     </div>
   )
